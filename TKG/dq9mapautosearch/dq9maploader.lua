539880aaf979f6f18df83460bb891681a7ac28f5
TKG <144775277+TKG-dq9@users.noreply.github.com>
2026-06-04 13:06:46 -0700

    Update dq9maploader.lua

--- TKG/dq9mapautosearch/dq9maploader.lua
+++ TKG/dq9mapautosearch/dq9maploader.lua
@@ -1,7 +1,7 @@
 local rankMin = {2, 56, 61, 76, 81, 101, 121, 141, 161, 181, 201, 221}
 local rankMax = {55, 60, 75, 80, 100, 120, 140, 160, 180, 200, 220, 248}
 
-local rank, seed
+local rank, seed, location
 
 local function getRank(quality)
     for i = 1, #rankMin do
@@ -13,14 +13,14 @@ local function getRank(quality)
 end
 
 local function updateLink()
-    if rank and seed then
-        local link = string.format("https://www.yabd.org/apps/dq9/grottodetails.php?map=%02X%04X", rank, seed)
+    if rank and seed and location then
+        local link = string.format("https://dqix.github.io/Grotto-Searcher/?id=%02X%04X", rank, seed)
         local file = io.open("dq9mapoutput.txt", "w")
         if file then
             file:write(link)
             file:close()
         end
-        print(link)
+        print(string.format("%02X %04X @ %02X", rank, seed, location))
     end
 end
 
@@ -31,5 +31,9 @@ end)
 
 memory.registerexec(0x020a5ee4, function()
    seed = memory.getregister("r1")
+end)
+
+memory.registerexec(0x020a5ef0, function()
+   location = memory.getregister("r0")
    updateLink()
 end)
